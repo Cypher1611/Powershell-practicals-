f0869097276fe9695f0fa854877dee770e316e34
Abhaysing Rajput <rajputabhay1608@gmail.com>
2024-09-13 12:00:53 +0530

    Create pratical 2 datastructure.ps1

---
+++ pratical 2 datastructure.ps1
@@ -0,0 +1,30 @@
+<#
+    Data Structure
+    -variables
+    -Arrays
+    -Hashtables
+
+#>
+cls
+#variables
+$a = 10
+$a.GetType()
+
+
+$b = 10.10
+$b.GetType()
+
+
+$c = "hello"
+$c.GetType()
+
+$d = Get-Date
+$d.GetType()
+$d.Day
+$d.DayOfWeek
+$d.DayOfYear
+$d.Minute
+$d.Second
+$d.Date
+
+(get-date).dayofweek
