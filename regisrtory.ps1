8f1d2534e65bc285348d55e3149d7f6ec7e1543e
Abhaysing Rajput <rajputabhay1608@gmail.com>
2024-09-14 11:44:13 +0530

    Update regisrtory.ps1

--- regisrtory.ps1
+++ regisrtory.ps1
@@ -3,3 +3,19 @@ Get-PSDrive
 Get-PSDrive -PSProvider Registry
 
 Get-ChildItem HKCU:\
+
+
+#creating a file in hkcu
+New-Item -Path HKCU:\ -Name "TestLTIb35.2" -force
+
+#creating a registory in abv folder
+New-ItemProperty -Path HKCU:\TestLTIb35.2 -Name "child" -Value "this is child" -force
+
+#changing the creating the valve of the key
+Set-ItemProperty -Path HKCU:\TestLTIb35.2 -Name "child" -Value "the last day " -Force
+
+#removing reg key
+Remove-ItemProperty -Path HKCU:\TestLTIb35.2 -Name "child" -Force
+
+#removing the reg folder
+Remove-Item -Path HKCU:\TestLTIb35.2 -Force
