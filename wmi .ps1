b4232bb483773f07169fbc1511b49b26ce144a8e
Abhaysing Rajput <rajputabhay1608@gmail.com>
2024-09-14 10:01:20 +0530

    Create wmi .ps1

---
+++ wmi .ps1
@@ -0,0 +1,18 @@
+#cim common information model
+
+#wmi windows management instrumentation
+
+Get-WmiObject -List
+Get-WmiObject -List | measure
+Get-WmiObject -List | Where-Object {$_.Name -match "^win32_"}
+Get-WmiObject -List | Where-Object {$_.Name -match "^win32_"} | measure
+
+Get-WmiObject -List | Where-Object {($_.Name -like "^win32_") -and ($_.Name -match "bios$")}
+Get-WmiObject -List | Where-Object {$_.Name -match "bios$"}
+
+Get-WmiObject -Class win32_BIOS
+Get-WmiObject -Class win32_Computersystem
+Get-WmiObject -Class win32_LogicalDisk
+Get-WmiObject -Class win32_OperatingSystem
+
+Get-WmiObject -Class win32_computersystem -ComputerName DC.Member1 | Format-Table
